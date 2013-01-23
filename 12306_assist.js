3409ea32c1005514a80ecfc7a064f569b4ecc069
hanchao <hanchao0123@hotmail.com>
2013-01-23 16:53:44 +0800

    input username and password for me

---
+++ 12306_assist.js
@@ -0,0 +1,21 @@
+﻿
+		
+var path = location.pathname;
+
+		
+console.log("loading");
+console.log(path);
+if (path == "/otsweb/main.jsp" || path == "/otsweb/") {
+	console.log("main");
+	
+
+}
+
+if ((path == "/otsweb/loginAction.do" && location.search != '?method=initForMy12306') || path == "/otsweb/login.jsp") {
+	//登录页
+	console.log("loginAction");
+	console.log($("#UserName").val());
+
+	$("#UserName").val("hanchao0123@hotmail.com");
+	$("#password").val("chao19860123");
+}
